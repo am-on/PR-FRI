081068d719ec0763204ce64947e83fe67e8ac746
am-on <igre@outlook.com>
2017-05-01 21:48:54 +0200

    part 1

--- dn-3/part1.py
+++ dn-3/part1.py
@@ -1,8 +1,6 @@
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
 from sklearn import linear_model
-from sklearn.metrics import explained_variance_score
 from sklearn.metrics import mean_absolute_error
 
 def learnPredictScore(model, trainX, trainY, testX, testY):
